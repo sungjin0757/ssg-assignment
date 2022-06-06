551c79e08642de4afbf16901b3ee671ef3b989a3
sungjin <sungjin0757@naver.com>
2022-06-06 17:24:32 +0900

    first commit

---
+++ src/main/java/com/ssgassignment/productinfoapi/ProductInfoApiApplication.java
@@ -0,0 +1,13 @@
+package com.ssgassignment.productinfoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductInfoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductInfoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssgassignment/productinfoapi/ProductInfoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssgassignment.productinfoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductInfoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
